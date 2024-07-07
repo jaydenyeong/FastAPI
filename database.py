525783b3f77e092443ceb3f70b674021333cb69c
jaydenyeong <76587099+jaydenyeong@users.noreply.github.com>
2024-07-07 23:46:02 +0800

    add pyodbc connection

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pyodbc
+import time
 
 engine = create_engine("mssql+pyodbc://localhost/fastapi?driver=ODBC+Driver+17+for+SQL+Server")
 
@@ -14,3 +16,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+server = 'localhost'
+database = 'fastapi'
+
+
+while True:
+    try:
+        conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;')
+        cursor = conn.cursor()
+        print("Connected to SQL Server")
+        break
+    except Exception as e:
+        print("Error: ", e)
+        time.sleep(2)
